ba94d9859edeb56e8f6e615cf046622d107a10db
DohKot <kiri1llaz@yandex.ru>
2025-06-07 17:56:36 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -79,16 +79,7 @@ double s(double& x, double& y)
 	res = sin(g);
 	return res;
 }
-double S(double& x, int terms)
-{
-    double result = 0.0;
-    for (int n = 0; n < terms; ++n)
-    {
-        double term = pow(-1, n) * pow(x, 2 * n + 1) / factorial(2 * n + 1);
-        result += term;
-    }
-    return result;
-}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -166,8 +157,7 @@ int main()
             }
             case 's':
             {
-                double g = Sum(x,y);
-                d = S(g, n);
+                d = s(x,y);
                 cout << d << endl;
                 break;
             }
